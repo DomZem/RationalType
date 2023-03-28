e4f2e846d1826d4a41e273e32671f763fd8d5965
Dominik Zemlik <dominik.zemlik.dev@gmail.com>
2023-03-28 23:54:48 +0200

    Add project files.

---
+++ RationalConsoleAppDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using RationalLib;
+using static System.Console;
+
+namespace RationalConsoleAppDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("hello world!");
+        }
+    }
+}
---
+++ RationalUnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace RationalUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
